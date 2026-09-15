26a6e0076a4af92f2590cfb3c9f3961b8286a395
Eliani <eliani@innova.do>
2026-09-15 12:35:04 -0400

    ACTUALIZADO

---
+++ interaccion.js
@@ -0,0 +1,8 @@
+function cambiarMensaje() {
+    document.getElementById("mensaje").textContent = "Gracias por ver un resumen de Pizza";
+}
+
+function mostraralerta(){
+alert("HOLA, APRENDI A COMO HACER UNA ALERTAAAAAA");
+
+}
